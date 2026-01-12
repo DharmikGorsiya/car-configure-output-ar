583af1cc33c8a62e2e23f9fb5b667fe8e51a0eb9
Dharmik Gorsiya <dhgorsiya@gmail.com>
2026-01-12 13:28:34 +0530

    AR First push

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "azure-car-configure-0.1";
+const contentToCache = [
+    "Build/car-configure-output-ar.loader.js",
+    "Build/car-configure-output-ar.framework.js",
+    "Build/car-configure-output-ar.data",
+    "Build/car-configure-output-ar.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
